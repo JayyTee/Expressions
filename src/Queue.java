cebbfccdf300f9bea3891eebb1df511961b76d85
Jaiquan <jt.rainey17@gmail.com>
2019-03-18 06:31:47 -0400

    "Added exception classes for full and empty"

--- src/Queue.java
+++ src/Queue.java
@@ -9,7 +9,14 @@ public class Queue extends LinkedList
     // Add item to rear of queue
     public void enqueue(String data)
     {
-        addRear(data);
+        try
+        {
+            addRear(data);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 
     //remove and return item at front of queue

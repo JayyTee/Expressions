9370bc60028667e643226ee743c8236cd2c01a5a
Jaiquan <jt.rainey17@gmail.com>
2019-03-17 21:01:23 -0400

    "bare node class created"

---
+++ src/Queue.java
@@ -0,0 +1,34 @@
+public class Queue extends LinkedList{
+
+    int maxSize = 0;
+    int size = 0;
+
+    Queue(int maxSize)
+    {
+        this.maxSize = maxSize;
+    }
+
+    // Add item to bottom of queue
+    public void enqueue()
+    {
+
+    }
+
+    //remove item from top of queue
+    public String Dequeue()
+    {
+
+    }
+
+    // Return size
+    private int size()
+    {
+        return this.size;
+    }
+
+    //return true if queue is full
+    public boolean full()
+    {
+
+    }
+}
